8762dae5cf9d65a28628457cd5ecbf5317ee7856
Prasetya Tegar Fitriaji <58251507+prasetyatf@users.noreply.github.com>
2022-01-30 20:27:46 +0700

    LogicOperator
    understanding difference between and, or, xor.

--- BasicPython.py
+++ BasicPython.py
@@ -38,3 +38,23 @@ print('ini list l', l[-1])#mengakses urutan list dr kanan
 
 p=[1,2]+[3,4]#menggabungkan list
 print('[1,2]+[3,4] = ', p)
+-------------------------------------------------------------
+#what i've learned this time is about logic operator
+-------------------------------------------------------------
+#and, or, xor
+a=True
+b=False
+print("NOTE: a=True, b= False")
+print("AND===")
+print("a and a = ",a and a)
+print("a and b = ",a and b)
+print("b and b = ",b and b)
+print("OR===")
+print("a or a = ",a or a)
+print("a or b = ",a or b)
+print("b or b = ",b or b)
+print("XOR=== nulisnya(^)")
+print("a xor a = ",a ^ a)
+print("a xor b = ",a ^ b)#jika hanya ada 1 yg benar mk benar
+print("b xor b = ",b ^ b)
+
