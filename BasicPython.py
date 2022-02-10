a15fcb68fdafb7726aeeecb7de0a02d3d5ad702f
Prasetya Tegar Fitriaji <58251507+prasetyatf@users.noreply.github.com>
2022-02-10 22:59:02 +0700

    DefFunction-BasicPython
    Variable operation: what can and what can not about value operation between difference data type, prove that python is procedural language, tupple, list and how to access it, string var is also a list, concatenate list
    Logic Operator: and, or, xor
    Comparasion Operator: >,<,=<,=>,==,!=,is,is not, is vs ==, what is object and what is literal
    List: access list, change list, add list, access nested list
    If else, elif
    Looping: for, while, break, continue, pass
    Def Function, argumen, return

--- BasicPython.py
+++ BasicPython.py
@@ -209,3 +209,42 @@ for i in range(7,10):
     print(i)
 else:
     print("selesai")
+-------------------------------------------------------------
+#what i've learned this time is about def(function):argumen, return
+-------------------------------------------------------------
+# fungsi dengan menggunakan argumen sederhana
+def siswa(nama):
+    print('siswa ini bernama:',nama)
+
+siswa('mario')
+
+# fungsi dengan menggunakan keywords arguments
+
+def guru(nama,pelajaran):
+    print('guru ini bernama:',nama)
+    print('mengajar:',pelajaran)
+
+guru(nama='popong',pelajaran='seni rupa')
+guru(pelajaran='olah raga',nama='endang')
+guru('olah raga','raihan') # ini contoh yang salah
+
+# fungsi dengan menggunakan default arguments
+
+def penjagaSekolah(nama,shift='siang',galak='tidak'):
+    print('penjaga sekolah ini bernama:', nama)
+    print('shiftnya:', shift)
+    print('galak?:', galak)
+
+penjagaSekolah('Entis')
+penjagaSekolah('Maman',shift='malam')
+penjagaSekolah('Asep',galak='Sangat')
+#penjagaSekolah(shift='malam',galak='iya') # menghasilkan error, nama belum dimasukkan
+
+def kuadrat(arg):
+    total = arg**2
+    print('kuadrat dari ', arg, 'adalah ', total)
+    return 'tes', total, arg
+
+kuadrat(3)
+a = kuadrat(5)
+print(a)#ini akan menghasilkan 'None' jika tidak ada return di dalam def
