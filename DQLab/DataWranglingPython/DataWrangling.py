d41cb6ab857dd437492ecbfee8caabe03d31af0e
Prasetya Tegar Fitriaji <58251507+prasetyatf@users.noreply.github.com>
2022-03-03 00:57:02 +0700

    Update DataWrangling.py

--- DQLab/DataWranglingPython/DataWrangling.py
+++ DQLab/DataWranglingPython/DataWrangling.py
@@ -10,3 +10,7 @@ print(csv_data.iloc[5])#akses data melalui baris, 5 adl index
 print(csv_data['Age'].iloc[1]) #akses kolom Age, baris ke-2
 print("Cuplikan Dataset:")
 print(csv_data.head())
+print(csv_data['Age'].iloc[5:10]) #mengakses kolom & baris sama seperti mengakses list
+print(csv_dataset.describe(include=all)) #mengetahui info stats scr cepat
+print(csv_data.describe(exclude=['O']) #mengabaikan data non-numerik
+print(csv_data.isnull()values.any()) #mengecek apa ada data kosong
