7757342411e3888ebce324ce07d92f9fe235d383
Prasetya Tegar Fitriaji <58251507+prasetyatf@users.noreply.github.com>
2022-03-03 01:42:20 +0700

    Update DataWrangling.py

--- DQLab/DataWranglingPython/DataWrangling.py
+++ DQLab/DataWranglingPython/DataWrangling.py
@@ -14,3 +14,12 @@ print(csv_data['Age'].iloc[5:10]) #mengakses kolom & baris sama seperti mengakse
 print(csv_dataset.describe(include=all)) #mengetahui info stats scr cepat
 print(csv_data.describe(exclude=['O']) #mengabaikan data non-numerik
 print(csv_data.isnull()values.any()) #mengecek apa ada data kosong
+
+#mengisi missing value dg mean/median. ingat di kursus lain, ada hal2 yang lebih detail ttg missing value
+print(csv_data.mean())
+print("Dataset yang masih terdapat nilai kosong ! :")
+print(csv_data.head(10))
+
+csv_data=csv_data.fillna(csv_data.mean()) #mengisi missing value dg mean. .median() utk mengisi dg median
+print("Dataset yang sudah diproses Handling Missing Values dengan Mean:")
+print(csv_data.head(10))
